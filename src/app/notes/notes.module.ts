798c0839be388685f35e6f7951e2d1e438e1227c
Jess4you <bojiefamoso@gmail.com>
2018-03-07 15:52:40 +0800

    second commit

--- src/app/notes/notes.module.ts
+++ src/app/notes/notes.module.ts
@@ -10,6 +10,16 @@ import { NotesListComponent } from './notes-list/notes-list.component';
 import { NoteDetailComponent } from './note-detail/note-detail.component';
 
 import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireModule } from 'angularfire2';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBk0ZtPBFBNwaeK3L8KX2T3zAPP1TmGb04",
+  authDomain: "oauth-93a9b.firebaseapp.com",
+  databaseURL: "https://oauth-93a9b.firebaseio.com",
+  projectId: "oauth-93a9b",
+  storageBucket: "oauth-93a9b.appspot.com",
+  messagingSenderId: "158385765734"
+};
 
 @NgModule({
   imports: [
@@ -21,7 +31,9 @@ import { AngularFirestoreModule } from 'angularfire2/firestore';
   declarations: [
     NotesListComponent,
     NoteDetailComponent,
+
   ],
   providers: [NoteService],
+  bootstrap: [NotesListComponent]
 })
 export class NotesModule { }
